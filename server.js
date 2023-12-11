f6b16a1bd381fae5c9b3812dce4392eccf132c20
Rafael <r.otavio.tenorio@gmail.com>
2023-12-11 18:56:40 -0300

    Allow Write Operations

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+
+// Uncomment to allow write operations
+ const fs = require('fs')
+ const path = require('path')
+ const filePath = path.join('db.json')
+ const data = fs.readFileSync(filePath, "utf-8");
+ const db = JSON.parse(data);
+ const router = jsonServer.router(db)
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
